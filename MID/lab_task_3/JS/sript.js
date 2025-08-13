408c03d7b795512fe840d099b0eee7d1caa5c930
HafijaHarunRitu <hafijaharunritu@gmail.com>
2025-08-13 19:39:44 +0600

    Forlders Added

---
+++ MID/lab_task_3/JS/sript.js
@@ -0,0 +1,39 @@
+function handlesubmit()
+{
+    var full_name = document.getElementById("full_name").value;
+    var age = document.getElementById("age").value;
+    var phonenumber= document.getElementById("phonenumber").value;
+    var eaddress = document.getElementById("eaddress").value;
+    var valid = true;
+
+    document.querySelectorAll('.error').forEach(e => e.innerText = "");
+    var valid = true;
+
+
+  if (!full_name) 
+  {
+    document.getElementById("fullNameError").innerText = "Full Name is required!";
+    valid = false;
+  }
+
+  if (!age) 
+  {
+    document.getElementById("ageError").innerText = "Age is required!";
+    valid = false;
+  }
+
+  if (!phonenumber) 
+  {
+    document.getElementById("phError").innerText = "Phone Number is required!";
+    valid = false;
+  }
+
+  if (!eaddress) 
+  {
+    document.getElementById("eaError").innerText = "Email Address is required!";
+    valid = false;
+  }
+
+  return valid;
+    
+}
